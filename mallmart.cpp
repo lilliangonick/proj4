1b39c7927c15da6f915e93d50477fb71dbef3357
lilliangonick <lillygonick@gmail.com>
2024-06-09 01:21:18 -0700

    implemented the create revision function, needs to implement the delimter check and optimizations

--- mallmart.cpp
+++ mallmart.cpp
@@ -3,20 +3,23 @@
 #include <fstream>
 #include <string>
 #include <cassert>
+#include <cmath>
 
 #include "Hashtable.h"
 
 using namespace std;
 
-#define N_LENGTH 8
-
-
+#define N_LENGTH 4
+#define CAPACITY 1000 // can change later
+#define ALL_FLAG true
 
 void createRevision(istream &fold, istream &fnew, ostream &frevision)
 {
     // Read in the entire contents of the old file into a string. Read the entire contents of the new file into another string.
     string oldFile;
     string newFile;
+    string reviseString;
+    HashTable table(CAPACITY);
 
     if (!fold)
     {
@@ -47,8 +50,53 @@ void createRevision(istream &fold, istream &fnew, ostream &frevision)
 
     // For all consecutive N-character sequences in the old file's string, insert that N-character sequence and the offset F where it was found in
     // the old file's string, into a table (e.g. hash table or binary search tree). You might use 8 for N, or maybe 16
-    
-    
+    for (size_t offset = 0; offset < oldFile.size(); ALL_FLAG ? offset += 1 : offset += N_LENGTH)
+    {
+        string substring = oldFile.substr(offset, N_LENGTH); // offset is where we slice (exclusive)
+        // IF STATEMENT IS INEFFICIENT FIX LATER
+        if (substring.size() != N_LENGTH)
+        {
+            break;
+        }
+        table.insert(substring, offset);
+    }
+
+    // Start processing the new file's string, starting from offset j=0, until j reaches the end of the string
+    // Look up the N-byte sequence which starts at offset j ([j,j+N-1]) in the new file's string in the table you created from the old file's string.
+    int offset = 0;
+    while (offset <= newFile.size())
+    {
+        // If we have another room to grab a N-character substring
+        if (offset + N_LENGTH <= newFile.size())
+        {
+            string substring = newFile.substr(offset, N_LENGTH);
+            pair<string, int> data = table.find(substring);
+            if (data.second != -1)
+            { // if the substring was in the hash table
+                // TODO See how long match is (OPTIMIZATION)
+                //  DO COPY INSTRUCTION
+                reviseString += "#" + to_string(data.second) + "," + to_string(N_LENGTH);
+                offset += N_LENGTH; // once optimization, substring.size()
+            }
+            else
+            {
+                // TODO MUST OPTIMIZE LATER (single char copying right now)
+                // TODO CHANGE DELIMINATOR (can not hard code the deliminator)
+                string deliminator = "~";
+                reviseString += "+" + deliminator + newFile[offset] + deliminator;
+                offset += 1;
+            }
+        }
+        else // if run out of n length strings
+        {
+            string substring = newFile.substr(offset, newFile.size() - offset);
+            string deliminator = "~";
+            reviseString += "+" + deliminator + substring + deliminator;
+            break;
+        }
+    }
+    cout << reviseString;
+    frevision << reviseString;
 };
 bool revise(istream &fold, istream &frevision, ostream &fnew);
 
@@ -110,9 +158,17 @@ int main()
     ifstream newfile("new.txt");
     ofstream outfile("output.txt");
     createRevision(oldfile, newfile, outfile);
-    testHashtable();
+    // testHashtable();
     return 0;
 }
 
 // to create/compile:
 // g++ *.cpp -o mallmart && ./mallmart
+
+// store the first char in each substring
+// iterate through whole string
+
+// delimator: look for a character that does not exist in the sequence we are adding/copying
+
+// first 
+// take in string and return something that is not in that string 
